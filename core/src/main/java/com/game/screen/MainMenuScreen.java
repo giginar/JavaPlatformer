86d9b001b6b6a11d11020d9665b645ee4560a70a
ykucukcinar <yigitkucukcinar@hotmail.com>
2025-04-17 21:21:12 +0300

    add main menu improvements

--- core/src/main/java/com/game/screen/MainMenuScreen.java
+++ core/src/main/java/com/game/screen/MainMenuScreen.java
@@ -1,86 +1,112 @@
 package com.game.screen;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.game.DeepDiveDrift;
 import com.game.manager.AudioManager;
 import com.game.manager.FontManager;
+import com.game.diver.Background;
 
 public class MainMenuScreen implements Screen {
 
     private final DeepDiveDrift game;
-    private SpriteBatch batch;
-    private BitmapFont font;
-    private GlyphLayout layout;
+    private final SpriteBatch batch;
+    private final BitmapFont font;
+    private final GlyphLayout layout;
+    private final ShapeRenderer shapeRenderer;
+    private final Background background;
 
-    private final String[] menuItems = {"Play Game", "Options", "Exit"};
+    private final String[] menuOptions = {"PLAY", "OPTIONS", "EXIT"};
     private int selectedIndex = 0;
+    private float transitionAlpha = 1f;
+    private boolean transitioning = false;
 
     public MainMenuScreen(DeepDiveDrift game) {
         this.game = game;
+        this.batch = new SpriteBatch();
+        this.font = FontManager.getMediumFont();
+        this.layout = new GlyphLayout();
+        this.shapeRenderer = new ShapeRenderer();
+        this.background = new Background();
     }
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
-        font = FontManager.getMediumFont();
-        layout = new GlyphLayout();
-
         AudioManager.playBackgroundMusic();
     }
 
     @Override
     public void render(float delta) {
-        handleInput();
-
         Gdx.gl.glClearColor(0, 0.1f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        background.update(delta);
+
         batch.begin();
-
-        layout.setText(font, "Deep Dive Drift");
-        font.draw(batch, layout, centerX(layout), Gdx.graphics.getHeight() - 100);
-
-        float startY = Gdx.graphics.getHeight() / 2f;
-        for (int i = 0; i < menuItems.length; i++) {
-            font.setColor(i == selectedIndex ? Color.YELLOW : Color.LIGHT_GRAY);
-            layout.setText(font, menuItems[i]);
-            font.draw(batch, layout, centerX(layout), startY - i * 40);
-        }
-
+        background.render(batch);
+        drawMenu();
         batch.end();
-    }
 
-    private void handleInput() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            selectedIndex = (selectedIndex - 1 + menuItems.length) % menuItems.length;
-            AudioManager.playSelect();
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
-            selectedIndex = (selectedIndex + 1) % menuItems.length;
-            AudioManager.playSelect();
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            AudioManager.playConfirm();
-            switch (selectedIndex) {
-                case 0 -> game.setScreen(new GameScreen());
-                case 1 -> game.setScreen(new OptionsScreen(game));
-                case 2 -> Gdx.app.exit();
+        if (!transitioning) {
+            handleInput();
+        } else {
+            transitionAlpha -= delta;
+            if (transitionAlpha <= 0) {
+                game.setScreen(new GameScreen());
             }
         }
     }
 
-    private float centerX(GlyphLayout layout) {
-        return (Gdx.graphics.getWidth() - layout.width) / 2f;
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            selectedIndex = (selectedIndex - 1 + menuOptions.length) % menuOptions.length;
+            AudioManager.playSelect();
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            selectedIndex = (selectedIndex + 1) % menuOptions.length;
+            AudioManager.playSelect();
+        } else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            AudioManager.playConfirm();
+            switch ((selectedIndex + 1) % menuOptions.length) {
+                case 0 -> transitioning = true; // PLAY
+                case 1 -> game.setScreen(new OptionsScreen(game)); // OPTIONS
+                case 2 -> Gdx.app.exit(); // EXIT
+            }
+        }
+    }
+
+    private void drawMenu() {
+        float startY = Gdx.graphics.getHeight() / 2f + 40;
+
+        for (int i = 0; i < menuOptions.length; i++) {
+            String option = menuOptions[i];
+            layout.setText(font, option);
+            float x = (Gdx.graphics.getWidth() - layout.width) / 2f;
+            float y = startY - i * 40;
+
+            font.setColor(i == selectedIndex ? Color.YELLOW : Color.LIGHT_GRAY);
+            font.draw(batch, layout, x, y);
+        }
+
+        if (transitioning) {
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.setColor(0, 0, 0, 1 - transitionAlpha);
+            shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            shapeRenderer.end();
+        }
     }
 
     @Override public void resize(int width, int height) {}
     @Override public void pause() {}
     @Override public void resume() {}
     @Override public void hide() {}
-
     @Override
     public void dispose() {
         batch.dispose();
+        shapeRenderer.dispose();
         font.dispose();
+        background.dispose();
     }
 }
